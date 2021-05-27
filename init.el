dc491bbc1e48ab27c6ebc97036ff4c858ff5daf2
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-05-27 16:35:34 +0200

    Revert "Merge branch 'agendaGTD'"
    This reverts commit 8b071cb940426d74d73b0b7e2816e74cf70afc36, reversing
    changes made to 9f6d6b01a31d320ca1ed549c250ca698bf3c4e9a.

--- init.el
+++ init.el
@@ -103,7 +103,6 @@
 (add-hook 'write-file-functions 'time-stamp) ; update time stamp when saving
 
 ;;;;; Org-agenda
-<<<<<<< HEAD
 (global-set-key (kbd "s-a a") 'org-agenda)
 (global-set-key (kbd "s-a f") 'org-cycle-agenda-files)
 (setq org-agenda-start-on-weekday nil)
@@ -125,24 +124,6 @@
 
 (setq org-refile-targets (quote ((nil :maxlevel . 9)
 																 (org-agenda-files :maxlevel . 9))))
-=======
-(setq org-agenda-files '("~/gtd/inbox.org"
-												 "~/gtd/gtd.org"
-												 "~/gtd/tickler.org"))
-
-(setq org-capture-templates '(("t" "Todo [inbox]" entry
-															 (file+headline "~/gtd/inbox.org" "Tasks")
-															 "* Todo %i%?")
-															("T" "Tickler" entry
-															 (file+headline "~/gtd/tickler.org" "Tickler")
-															 "* %i%? \n %U")))
-
-(setq org-refile-targets '(("~/gtd/gtd.org" :maxlevel . 3)
-													 ("~/gtd/someday.org" :level . 1)
-													 ("~/gtd/tickler.org" :maxlevel . 2)))
-
-(setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
->>>>>>> agendaGTD
 
 ;;;; Flyspell
 (add-hook 'org-mode-hook 'flyspell-mode)

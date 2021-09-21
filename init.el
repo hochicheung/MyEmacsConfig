e7e1e1e1e8054c956719322df31a51590afab41e
Sam Cheung <hochi.cheung.sam@gmail.com>
2021-09-21 15:01:14 +0200

    added new function

--- init.el
+++ init.el
@@ -146,9 +146,26 @@
 
 (setq org-todo-keywords '((sequence "TODO(t)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
 
-(setq org-agenda-custom-commands
-			'(("o" "At the office" tags-todo "@office"
-				 ((org-agenda-overriding-header "Office")))))
+(setq org-agenda-custom-commands 
+      '(("o" "At the office" tags-todo "@office"
+         ((org-agenda-overriding-header "Office")
+          (org-agenda-skip-function #'my/org-agenda-skip-all-siblings-but-first)))))
+
+(defun my/org-agenda-skip-all-siblings-but-first ()
+  "Skip all but the first non-done entry."
+  (let (should-skip-entry)
+    (unless (org-current-is-todo)
+      (setq should-skip-entry t))
+    (save-excursion
+      (while (and (not should-skip-entry) (org-goto-sibling t))
+        (when (org-current-is-todo)
+          (setq should-skip-entry t))))
+    (when should-skip-entry
+      (or (outline-next-heading)
+          (goto-char (point-max))))))
+
+(defun org-current-is-todo ()
+  (string= "TODO" (org-get-todo-state)))
 
 ;;;; Flyspell
 (add-hook 'org-mode-hook 'flyspell-mode)

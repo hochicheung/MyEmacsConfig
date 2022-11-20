81bcc19548593478854c38d9a57d485f2d58db97
Sam Cheung <hochi.cheung.sam@gmail.com>
2022-11-20 17:09:02 +0100

    Disabled byte-compilation of config file

--- init.el
+++ init.el
@@ -2,6 +2,11 @@
 (when (string< emacs-version "27")
 	(load-file (concat user-emacs-directory "early-init.el")))
 
-(let ((byte-compile-warnings nil))
-	(byte-recompile-file (concat user-emacs-directory "config.el") nil 0 t))
+;; Load config
+(load-file (concat user-emacs-directory "config.el"))
+
+;; Byte-compile config.el
+;;(let ((byte-compile-warnings nil))
+	;;(byte-recompile-file (concat user-emacs-directory "config.el") nil 0 t))
+
 (put 'list-timers 'disabled nil)
